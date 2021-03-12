1c7c44b9528e126eba904f90757a23ddf1e8cea3
Karthik Srinivasan <email.karthik.srinivasan@gmail.com>
2021-03-11 19:56:18 -0500

    Initial Spring Boot Template

---
+++ src/test/java/com/flexion/coding/challenge/UnitConversion/UnitConversionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flexion.coding.challenge.UnitConversion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitConversionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
