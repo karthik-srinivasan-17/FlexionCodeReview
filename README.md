aa8a0b5a1e9b354982e969c04e2d7df11e745ede
Karthik Srinivasan <email.karthik.srinivasan@gmail.com>
2021-03-14 19:59:40 -0400

    Generated java Docs, Updated Read Me file. Changed build configuration for local and Prod servers.

--- README.md
+++ README.md
@@ -1,1 +1,76 @@
 # FlexionCodeReview
+
+The project is written in Client - Server Arichtecture model
+
+Frontend of the application is written in AngularJS, Backend of the Application is written in Spring Boot
+
+Front End Pre-Installation  Requirment
+Angular CLI: 12.0.0-next.4
+Node: 12.16.3
+NPM : 6.14.5
+
+Backend Pre-Installation  Requirement
+openjdk version "1.8.0_282"
+Apache Maven 3.1.1 
+
+To test the front end requirement please open command prompt or terminal these commands
+Angular CLI ---- "ng v"
+Node ----------- "node -v"
+NPM ------------ "npm -v"
+
+To test the Backend end requirement please open command prompt or terminal these commands
+openjdk ----------- "java -version"
+Apache Maven ------ "mvn -v"
+
+
+Backend java codes are present in \FlexionCodeReview\src\main\java folder
+Frontend angular codes are present in \FlexionCodeReview\src\main\ui folder
+
+To run application in local environment 
+
+Backend Application. 
+
+1) Change the packing to jar in pom.xml file under properties element. 
+2) Open command prompt or terminal, go to \FlexionCodeReview folder, run "mvn clean install"
+3) All the junit test cases should pass, and Unit-Conversion-2.0.jar file should be created under \FlexionCodeReview\src\target folder. 
+4) on the command prompt or terminal, go to \FlexionCodeReview\src\target  folder and run "java -jar Unit-Conversion-2.0.jar"
+5) Open any browser and go to "http://localhost:8082/", You should see a HTML page which "This is index.html". This confirms backend application is running properly. 
+
+Front End Application. 
+1) Open command prompt or terminal, go to \FlexionCodeReview\src\main\ui
+2) Run the command "npm start"
+3) Open any browser and go to "http://localhost:4200/", You should see the Unit Converison Application UI. 
+
+
+To run application in PROD environment 
+
+Backend Application. 
+1) Change the packing to war in pom.xml file under properties element. 
+2) Open command prompt or terminal, go to \FlexionCodeReview folder, run command "mvn clean install"
+3) All the junit test cases should pass, and Unit-Conversion-2.0.war file should be created under \FlexionCodeReview\src\target folder.
+4) Deploy the War file on a web server of your choice. Tomcat is used here but feel free to use any other webserver. 
+5) Under Tomcat installtion directory, copy the  war file under webapps folder, stop and start Tomcat the server. Stop and Start script are the under bin folder in tomcat directory. 
+6) Open any browser and go to "http://WebServerDNS:8080/UnitConversion-2.0/", You should see a HTML page which "This is index.html". This confirms backend application is running properly. ( Please note that "WebServerDNS" in the URL is the hostname of prod server, please change it accordingly) 
+
+Front End Application. 
+1) Open command prompt or terminal or explorer, Go to \FlexionCodeReview\src\main\ui folder, Under the folder, please open the file "proxy-prod.conf.json" using notepad or vi editor or any editor of your choice. 
+2) Change the target url to above URL where the WAR file was deployed like  "target": "http://WebServerDNS:8080/UnitConversion-2.0/".
+(Please note that "WebServerDNS" in the URL is the hostname of prod server, please change it accordingly) 
+3) Open command prompt or terminal, go to \FlexionCodeReview\src\main\ui
+4) Run the command "npm run-script build". A dist folder will be created under \FlexionCodeReview\src\main\ui directory. 
+5) Copy the ui folder under dist folder, and deploy it on the Webserver of your choice. Tomcat is used here but feel free to use any other webserver. 
+5) Open any browser and go to "http://WebServerDNS:8080/ui/", You should see the Unit Converison Application UI. ( Please note that "WebServerDNS" in the URL is the hostname of prod server, please change it accordingly)
+
+
+
+Common issues faced. 
+
+1) My UI application deployed on Prod server is not working. 
+a) Please make sure the target URL in  "proxy-prod.conf.json" is updated properly. 
+b) If you are running Tomcat, please make sure the value "<Valve className="org.apache.catalina.valves.rewrite.RewriteValve" />" is present in host element of server.xml under $TomcatDirectory\conf\ folder
+b) Also If you are running Tomcat, please make you have the rewrite.config file under $TomcatDirectory\conf\Catalina\localhost is present. Please add the rule as shown below. 
+RewriteCond %{REQUEST_PATH} !-f
+RewriteRule ^/ui/(.*) /ui/index.html 
+
+
+
--- src/test/java/com/flexion/coding/challenge/unitconversion/controller/UnitConversionControllerTests.java
+++ src/test/java/com/flexion/coding/challenge/unitconversion/controller/UnitConversionControllerTests.java
@@ -2,23 +2,73 @@ package com.flexion.coding.challenge.unitconversion.controller;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
+import com.flexion.coding.challenge.unitconversion.AppController;
+import com.flexion.coding.challenge.unitconversion.UnitConversionApplication;
 import com.flexion.coding.challenge.unitconversion.entity.Entries;
 import com.flexion.coding.challenge.unitconversion.utilities.ConversionType;
 import com.flexion.coding.challenge.unitconversion.utilities.Temperature;
+import com.flexion.coding.challenge.unitconversion.utilities.Volumes;
 
 @SpringBootTest
 class UnitConversionControllerTests {
 	
 	@Autowired
 	UnitConversionController ucController;
+	@Autowired
+	AppController appController;
+	
+	@Test
+    public void testAppController()
+    {
+		String htmlPage = appController.loadUI();
+		assertTrue(htmlPage.equals("forward:/index.html"));
+    }
+	
+	
+	@Test
+    public void test()
+    {
+		UnitConversionApplication.main(new String[]{
+                "--spring.main.web-environment=false",
+                "--spring.autoconfigure.exclude=blahblahblah",
+                // Override any other environment properties according to your needs
+        });
+    }
+	
+	 @Test
+	  public void testGetAllEntries() {
+		 Entries sampleEntry1 = new Entries();
+		 sampleEntry1.setAskedValue("1.0");
+		 sampleEntry1.setConversionType(ConversionType.TEMPERATURE.toString());
+		 sampleEntry1.setFromMeasurement(Temperature.CELSIUS.toString());
+		 sampleEntry1.setToMeasurement(Temperature.FAHRENHEIT.toString());
+		 sampleEntry1.setWrittenValue("33.8");
+		 ucController.checkConversion(sampleEntry1);
+		 Entries sampleEntry2 = new Entries();
+		 sampleEntry2.setAskedValue("2.0");
+		 sampleEntry2.setConversionType(ConversionType.VOLUME.toString());
+		 sampleEntry2.setFromMeasurement(Volumes.GALLON.toString());
+		 sampleEntry2.setToMeasurement(Volumes.LITER.toString());
+		 sampleEntry2.setWrittenValue("2.78");
+		 ucController.checkConversion(sampleEntry2);
+		 ResponseEntity<?> responseEntry =  ucController.listEntries();
+		 assertEquals(responseEntry.getStatusCodeValue(),HttpStatus.OK.value());
+		 List<Entries> resultEntry = (List<Entries>) responseEntry.getBody();
+		 System.out.println(responseEntry.getBody().toString());
+		 assertEquals(resultEntry.size(),6);
+	  }
 
 	 @Test
-	  public void testCheckConversionCorrect() {
+	  public void testTemperatureCorrect() {
 		 Entries sampleEntry = new Entries();
 		 //sampleEntry.setId(1L);
 		 sampleEntry.setAskedValue("1.0");
@@ -34,7 +84,7 @@ class UnitConversionControllerTests {
 	  }
 	 
 	 @Test
-	  public void testCheckConversionIncorrect() {
+	  public void testTemparatureIncorrect() {
 		 Entries sampleEntry = new Entries();
 		 //sampleEntry.setId(2L);
 		 sampleEntry.setAskedValue("2.0");
@@ -48,4 +98,107 @@ class UnitConversionControllerTests {
 		 System.out.println(responseEntry.getBody().toString());
 		 assertEquals(resultEntry.getResult(),"INCORRECT");
 	  }
+	 
+	 
+	 @Test
+	  public void testVolumeCorrect() {
+		 Entries sampleEntry = new Entries();
+		 //sampleEntry.setId(1L);
+		 sampleEntry.setAskedValue("1.0");
+		 sampleEntry.setConversionType(ConversionType.VOLUME.toString());
+		 sampleEntry.setFromMeasurement(Volumes.GALLON.toString());
+		 sampleEntry.setToMeasurement(Volumes.LITER.toString());
+		 sampleEntry.setWrittenValue("3.78");
+		 ResponseEntity<?> responseEntry =  ucController.checkConversion(sampleEntry);
+		 assertEquals(responseEntry.getStatusCodeValue(),HttpStatus.OK.value());
+		 Entries resultEntry = (Entries) responseEntry.getBody();
+		 System.out.println(responseEntry.getBody().toString());
+		 assertEquals(resultEntry.getResult(),"CORRECT");
+	  }
+	 
+	 @Test
+	  public void testVolumeIncorrect() {
+		 Entries sampleEntry = new Entries();
+		 //sampleEntry.setId(2L);
+		 sampleEntry.setAskedValue("2.0");
+		 sampleEntry.setConversionType(ConversionType.VOLUME.toString());
+		 sampleEntry.setFromMeasurement(Volumes.GALLON.toString());
+		 sampleEntry.setToMeasurement(Volumes.LITER.toString());
+		 sampleEntry.setWrittenValue("2.78");
+		 ResponseEntity<?> responseEntry =  ucController.checkConversion(sampleEntry);
+		 assertEquals(responseEntry.getStatusCodeValue(),HttpStatus.OK.value());
+		 Entries resultEntry = (Entries) responseEntry.getBody();
+		 System.out.println(responseEntry.getBody().toString());
+		 assertEquals(resultEntry.getResult(),"INCORRECT");
+	  }
+	 
+	 @Test
+	  public void testTemperatureINVALID() {
+		 Entries sampleEntry = new Entries();
+		 //sampleEntry.setId(1L);
+		 sampleEntry.setAskedValue("1.0");
+		 sampleEntry.setConversionType(ConversionType.TEMPERATURE.toString());
+		 sampleEntry.setFromMeasurement(Temperature.FAHRENHEIT.toString());
+		 sampleEntry.setToMeasurement(Temperature.FAHRENHEIT.toString());
+		 sampleEntry.setWrittenValue("33.8");
+		 ResponseEntity<?> responseEntry =  ucController.checkConversion(sampleEntry);
+		 assertEquals(responseEntry.getStatusCodeValue(),HttpStatus.OK.value());
+		 Entries resultEntry = (Entries) responseEntry.getBody();
+		 System.out.println(responseEntry.getBody().toString());
+		 assertEquals(resultEntry.getResult(),"INVALID");
+	  }
+	 
+	 @Test
+	  public void testInvalidTemperature() {
+		 Entries sampleEntry = new Entries();
+		 sampleEntry.setAskedValue("1.0");
+		 sampleEntry.setConversionType(ConversionType.TEMPERATURE.toString());
+		 sampleEntry.setFromMeasurement("Garbage Value");
+		 sampleEntry.setToMeasurement(Temperature.FAHRENHEIT.toString());
+		 sampleEntry.setWrittenValue("33.8");
+		 ResponseEntity<?> responseEntry =  ucController.checkConversion(sampleEntry);
+		 assertEquals(responseEntry.getStatusCodeValue(),HttpStatus.OK.value());
+		 Entries resultEntry = (Entries) responseEntry.getBody();
+		 System.out.println(responseEntry.getBody().toString());
+		 assertEquals(resultEntry.getResult(),"INVALID");
+		 Entries sampleEntry1 = new Entries();
+		 sampleEntry1.setAskedValue("1.0");
+		 sampleEntry1.setConversionType(ConversionType.TEMPERATURE.toString());
+		 sampleEntry1.setFromMeasurement(Temperature.FAHRENHEIT.toString());
+		 sampleEntry1.setToMeasurement("Garbage Value");
+		 sampleEntry1.setWrittenValue("33.8");
+		 ResponseEntity<?> responseEntry1 =  ucController.checkConversion(sampleEntry);
+		 assertEquals(responseEntry1.getStatusCodeValue(),HttpStatus.OK.value());
+		 Entries resultEntry1 = (Entries) responseEntry1.getBody();
+		 System.out.println(responseEntry1.getBody().toString());
+		 assertEquals(resultEntry1.getResult(),"INVALID");
+	  }
+	 
+	 @Test
+	  public void testInvalideType() {
+		 Entries sampleEntry = new Entries();
+		 //sampleEntry.setId(1L);
+		 sampleEntry.setAskedValue("1.0");
+		 sampleEntry.setConversionType(null);
+		 sampleEntry.setFromMeasurement(Temperature.KELVIN.toString());
+		 sampleEntry.setToMeasurement(Temperature.FAHRENHEIT.toString());
+		 sampleEntry.setWrittenValue("33.8");
+
+		 Assertions.assertThrows(Exception.class, () ->  ucController.checkConversion(sampleEntry));
+		 Entries sampleEntry1 = new Entries();
+		 //sampleEntry.setId(1L);
+		 sampleEntry1.setAskedValue("1.0");
+		 sampleEntry1.setConversionType("Garbage Value");
+		 sampleEntry1.setFromMeasurement(Temperature.KELVIN.toString());
+		 sampleEntry1.setToMeasurement(Temperature.FAHRENHEIT.toString());
+		 sampleEntry1.setWrittenValue("33.8");
+
+		 ResponseEntity<?> responseEntry1 =  ucController.checkConversion(sampleEntry1);
+		 assertEquals(responseEntry1.getStatusCodeValue(),HttpStatus.OK.value());
+		 Entries resultEntry1 = (Entries) responseEntry1.getBody();
+		 System.out.println(responseEntry1.getBody().toString());
+		 assertEquals(resultEntry1.getResult(),"INVALID");
+	  }
+	 
+	
 }
